9b9a82dcdc487838fdf11c2194edfca189ae24e6
dwiky1922 <a710230033@student.ums.ac.id>
2024-06-21 21:21:32 +0700

    String Formating

---
+++ Day 7/String Formating.py
@@ -0,0 +1,9 @@
+kampus = 'UMS' 
+lokasi = 'Surakarta' 
+print('Selamat Datang di {}, kampus {} tercinta'.format(lokasi,kampus))
+  
+string = "Nilai {1} {0}, adalah {2}%" 
+print(string.format('Irawan', 'Arthur', 75)) 
+
+string_dua = '{band} menciptakan lagu {lagu} pada tahun {tahun}' 
+print(string_dua.format(band='Barasuara',lagu='Mengunci Ingatan',tahun= '2015')) 
